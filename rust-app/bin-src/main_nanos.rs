22726aba43f288b84a3a5ab5a44cf97c8ca94a07
John Ericson <John.Ericson@Obsidian.Systems>
2022-02-03 23:46:09 +0000

    Remove some stray old names

--- rust-app/bin-src/main_nanos.rs
+++ rust-app/bin-src/main_nanos.rs
@@ -22,7 +22,7 @@ extern "C" fn sample_main() {
     let mut idle_menu = RootMenu::new([ concat!("Pocket ", env!("CARGO_PKG_VERSION")), "Exit" ]);
     let mut busy_menu = RootMenu::new([ "Working...", "Cancel" ]);
 
-    info!("Kadena app {}", env!("CARGO_PKG_VERSION"));
+    info!("Pocket app {}", env!("CARGO_PKG_VERSION"));
 
     loop {
         // Draw some 'welcome' screen
@@ -141,7 +141,7 @@ fn handle_apdu(comm: &mut io::Comm, ins: Ins, parser: &mut ParsersState) -> Resu
     match ins {
         Ins::GetVersion => {
             comm.append(&[env!("CARGO_PKG_VERSION_MAJOR").parse().unwrap(), env!("CARGO_PKG_VERSION_MINOR").parse().unwrap(), env!("CARGO_PKG_VERSION_PATCH").parse().unwrap()]);
-            comm.append(b"Kadena");
+            comm.append(b"Pocket");
         }
         Ins::GetPubkey => {
             run_parser_apdu::<_, Bip32Key>(parser, get_get_address_state, &GET_ADDRESS_IMPL, comm)?
@@ -150,7 +150,7 @@ fn handle_apdu(comm: &mut io::Comm, ins: Ins, parser: &mut ParsersState) -> Resu
             run_parser_apdu::<_, SignParameters>(parser, get_sign_state, &SIGN_IMPL, comm)?
         }
         Ins::GetVersionStr => {
-            comm.append(concat!("Kadena ", env!("CARGO_PKG_VERSION")).as_ref());
+            comm.append(concat!("Pocket ", env!("CARGO_PKG_VERSION")).as_ref());
         }
         Ins::Exit => nanos_sdk::exit_app(0),
     }
