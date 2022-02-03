765d553bd9bb70e7d8abfe7513d80467e463fcaa
John Ericson <John.Ericson@Obsidian.Systems>
2022-02-03 23:59:01 +0000

    Use `Option::insert` in a few places

--- rust-app/src/implementation.rs
+++ rust-app/src/implementation.rs
@@ -37,9 +37,9 @@ pub const GET_ADDRESS_IMPL: GetAddressImplT =
 
         final_accept_prompt(&[])?;
 
-        *destination=Some(ArrayVec::new());
-        destination.as_mut()?.try_push(u8::try_from(33).ok()?).ok()?;
-        destination.as_mut()?.try_extend_from_slice(&key).ok()?;
+        let rv = destination.insert(ArrayVec::new());
+        rv.try_push(u8::try_from(key.len()).ok()?).ok()?;
+        rv.try_extend_from_slice(&key).ok()?;
         Some(())
     }));
 
@@ -174,9 +174,8 @@ pub const SIGN_IMPL: SignImplT = Action(
         // By the time we get here, we've approved and just need to do the signature.
         final_accept_prompt(&[])?;
         let sig = detecdsa_sign(hash.as_ref()?, key.as_ref()?)?;
-        let mut rv = ArrayVec::<u8, 128>::new();
+        let rv = destination.insert(ArrayVec::new());
         rv.try_extend_from_slice(&sig).ok()?;
-        *destination = Some(rv);
         Some(())
     }),
 );
