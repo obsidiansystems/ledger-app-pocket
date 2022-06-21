87d39a6cbdff649ee870cb96f120a50b896dbaa0
Divam <dfordivam@gmail.com>
2022-06-21 21:04:51 +0900

    Use the "ledger-app" nix APIs from ledger-platform

--- default.nix
+++ default.nix
@@ -1,124 +1,19 @@
 rec {
   ledger-platform = import ./dep/ledger-platform {};
 
-  inherit (ledger-platform)
-    lib
-    pkgs ledgerPkgs
-    crate2nix
-    buildRustCrateForPkgsLedger
-    buildRustCrateForPkgsWrapper
-    ;
-
-  makeApp = { rootFeatures ? [ "default" ], release ? true }: import ./Cargo.nix {
-    inherit rootFeatures release;
-    pkgs = ledgerPkgs;
-    buildRustCrateForPkgs = pkgs: let
-      fun = buildRustCrateForPkgsWrapper
-        pkgs
-        ((buildRustCrateForPkgsLedger pkgs).override {
-          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-            pocket = attrs: let
-              sdk = lib.findFirst (p: lib.hasPrefix "rust_nanos_sdk" p.name) (builtins.throw "no sdk!") attrs.dependencies;
-            in {
-              preHook = ledger-platform.gccLibsPreHook;
-              extraRustcOpts = attrs.extraRustcOpts or [] ++ [
-                "-C" "link-arg=-T${sdk.lib}/lib/nanos_sdk.out/script.ld"
-                "-C" "linker=${pkgs.stdenv.cc.targetPrefix}clang"
-              ];
-            };
-          };
-        });
-    in
-      args: fun (args // lib.optionalAttrs pkgs.stdenv.hostPlatform.isAarch32 {
-        dependencies = map (d: d // { stdlib = true; }) [
-          ledger-platform.ledgerCore
-          ledger-platform.ledgerCompilerBuiltins
-        ] ++ args.dependencies;
-      });
+  inherit (ledger-platform) pkgs ;
+  ledger-app = ledger-platform.ledger-app {
+    appName = "pocket";
+    appGif = ./rust-app/pocket.gif;
+    appToml = ./rust-app/Cargo.toml;
+    cargoNix = import ./Cargo.nix;
+    testPackage = (import ./ts-tests/override.nix { inherit pkgs; }).package;
   };
 
-  app = makeApp {};
-  app-with-logging = makeApp {
-    release = false;
-    rootFeatures = [ "default" "speculos" "extra_debug" ];
-  };
-
-  # For CI
-  rootCrate = app.rootCrate.build;
-  rootCrate-with-logging = app-with-logging.rootCrate.build;
-
-  tarSrc = ledgerPkgs.runCommandCC "tarSrc" {
-    nativeBuildInputs = [
-      ledger-platform.cargo-ledger
-      ledger-platform.ledgerRustPlatform.rust.cargo
-    ];
-  } (ledger-platform.cargoLedgerPreHook + ''
-
-    cp ${./rust-app/Cargo.toml} ./Cargo.toml
-    # So cargo knows it's a binary
-    mkdir src
-    touch src/main.rs
-
-    RUSTC_BOOTSTRAP=1 cargo-ledger --use-prebuilt ${rootCrate}/bin/pocket --hex-next-to-json
-
-    mkdir -p $out/pocket
-    cp app.json app.hex $out/pocket
-    cp ${./tarball-default.nix} $out/pocket/default.nix
-    cp ${./tarball-shell.nix} $out/pocket/shell.nix
-    cp ${./rust-app/pocket.gif} $out/pocket/pocket.gif
-  '');
-
-  tarball = pkgs.runCommandNoCC "app-tarball.tar.gz" { } ''
-    tar -czvhf $out -C ${tarSrc} pocket
-  '';
-
-  loadApp = pkgs.writeScriptBin "load-app" ''
-  #!/usr/bin/env bash
-    cd ${tarSrc}/pocket
-    ${ledger-platform.ledgerctl}/bin/ledgerctl install -f ${tarSrc}/pocket/app.json
-  '';
+  inherit (ledger-app) loadApp tarball test;
 
   appShell = pkgs.mkShell {
     packages = [ loadApp ledger-platform.generic-cli pkgs.jq pocket-core pocket-cli-cmd-renamed ];
-  };
-
-  testPackage = (import ./ts-tests/override.nix { inherit pkgs; }).package;
-
-  testScript = pkgs.writeShellScriptBin "mocha-wrapper" ''
-    cd ${testPackage}/lib/node_modules/*/
-    export NO_UPDATE_NOTIFIER=true
-    exec ${pkgs.nodejs-14_x}/bin/npm --offline test -- "$@"
-  '';
-
-  runTests = { appExe ? rootCrate + "/bin/pocket" }: pkgs.runCommandNoCC "run-tests" {
-    nativeBuildInputs = [
-      pkgs.wget ledger-platform.speculos.speculos testScript
-    ];
-  } ''
-    RUST_APP=${rootCrate}/bin/*
-    echo RUST APP IS $RUST_APP
-    # speculos -k 2.0 $RUST_APP --display headless &
-    mkdir $out
-    (
-    speculos -k 2.0 ${appExe} --display headless &
-    SPECULOS=$!
-
-    until wget -O/dev/null -o/dev/null http://localhost:5000; do sleep 0.1; done;
-
-    ${testScript}/bin/mocha-wrapper
-    rv=$?
-    kill -9 $SPECULOS
-    exit $rv) | tee $out/short |& tee $out/full
-    rv=$?
-    cat $out/short
-    exit $rv
-  '';
-
-  # test-with-loging = runTests {
-  #   appExe = rootCrate-with-logging + "/bin/pocket";
-  # };
-  test = runTests {
-    appExe = rootCrate + "/bin/pocket";
   };
 
   inherit (pkgs.nodePackages) node2nix;
--- tarball-shell.nix
+++
@@ -1,8 +0,0 @@
-let
-  ledgerPlatform = import (fetchTarball "https://github.com/obsidiansystems/ledger-platform/archive/develop.tar.gz") {};
-  pkgs = ledgerPlatform.pkgs;
-  load-app = import ./.;
-in
-  pkgs.mkShell {
-    buildInputs = [load-app];
-  }
