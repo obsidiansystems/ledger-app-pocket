2dd1656189e3104b64d010ed7593dd0611e2d4db
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-07-14 20:06:18 -0400

    Include the build docker image in CI.

--- release.nix
+++ release.nix
@@ -3,4 +3,5 @@ in
   {
     inherit (self) tarball loadApp pocket-core test;
     generic-cli = self.ledger-platform.generic-cli;
+    build-docker-recompressed = self.ledger-platform.build-docker-recompressed;
   }
