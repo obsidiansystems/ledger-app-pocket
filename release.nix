eb9df72e2376843ed8af433c41ae80caabf20eea
Divam <dfordivam@gmail.com>
2023-06-27 17:57:43 +0900

    Disable test and stack-check of nanos with logging

--- release.nix
+++ release.nix
@@ -14,10 +14,10 @@ in {
 }
   # Hack until CI will traverse contents
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--" + n)) perSystem.x86_64-linux
-  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) perSystem.x86_64-linux.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-linux.nanos ["test-with-logging" "stack-check-with-logging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanox--" + n)) perSystem.x86_64-linux.nanox
   // lib.mapAttrs' (n: lib.nameValuePair ("linux--nanosplus--" + n)) perSystem.x86_64-linux.nanosplus
   // lib.mapAttrs' (n: lib.nameValuePair ("macos--" + n)) perSystem.x86_64-darwin
-  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) perSystem.x86_64-darwin.nanos
+  // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanos--" + n)) (builtins.removeAttrs perSystem.x86_64-darwin.nanos ["test-with-logging" "stack-check-with-logging"])
   // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanox--" + n)) perSystem.x86_64-darwin.nanox
   // lib.mapAttrs' (n: lib.nameValuePair ("macos--nanosplus--" + n)) perSystem.x86_64-darwin.nanosplus
