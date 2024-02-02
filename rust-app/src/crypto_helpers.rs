2517d0e89cc36b0babe28e8c4cddbc83ae20dd29
Divam <dfordivam@gmail.com>
2024-02-02 13:16:29 +0900

    Fix crypto_helpers.rs

--- rust-app/src/crypto_helpers.rs
+++ rust-app/src/crypto_helpers.rs
@@ -2,16 +2,16 @@
 use core::fmt;
 use ledger_crypto_helpers::common::*;
 use ledger_crypto_helpers::eddsa::*;
-use nanos_sdk::bindings::*;
-use nanos_sdk::io::SyscallError;
+use ledger_secure_sdk_sys::*;
+use ledger_device_sdk::io::SyscallError;
 
 // Public Key Hash type; update this to match the target chain's notion of an address and how to
 // format one.
 
 pub struct PKH(pub [u8; 20]);
 
-impl Address<PKH, nanos_sdk::ecc::ECPublicKey<65, 'E'>> for PKH {
-    fn get_address(key: &nanos_sdk::ecc::ECPublicKey<65, 'E'>) -> Result<Self, SyscallError> {
+impl Address<PKH, ledger_device_sdk::ecc::ECPublicKey<65, 'E'>> for PKH {
+    fn get_address(key: &ledger_device_sdk::ecc::ECPublicKey<65, 'E'>) -> Result<Self, SyscallError> {
         get_pkh(key)
     }
     fn get_binary_address(&self) -> &[u8] {
@@ -20,15 +20,15 @@ impl Address<PKH, nanos_sdk::ecc::ECPublicKey<65, 'E'>> for PKH {
 }
 
 #[allow(dead_code)]
-pub fn get_pkh(key: &nanos_sdk::ecc::ECPublicKey<65, 'E'>) -> Result<PKH, SyscallError> {
+pub fn get_pkh(key: &ledger_device_sdk::ecc::ECPublicKey<65, 'E'>) -> Result<PKH, SyscallError> {
     let mut public_key_hash = [0; 32];
     let key_bytes = ed25519_public_key_bytes(key);
     unsafe {
-        let _len: size_t = cx_hash_sha256(
+        let _len = cx_hash_sha256(
             key_bytes.as_ptr(),
-            key_bytes.len() as u32,
+            key_bytes.len(),
             public_key_hash.as_mut_ptr(),
-            public_key_hash.len() as u32,
+            public_key_hash.len(),
         );
     }
     let mut rv = PKH([0; 20]);
